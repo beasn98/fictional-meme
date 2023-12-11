f75fdd737bba4ab9b592898f38dcda1bb75b2dfd
Eisen <eisengong@gmail.com>
2023-12-11 10:08:30 -0800

    Sadge^2

--- src/Day01.kt
+++ src/Day01.kt
@@ -3,144 +3,109 @@ fun main() {
     val numForward = hashMapOf<String, Int>("one" to 1, "two" to 2, "three" to 3, "four" to 4, "five" to 5, "six" to 6, "seven" to 7, "eight" to 8, "nine" to 9)
     val numBackward = hashMapOf<String, Int>("eno" to 1, "owt" to 2, "eerht" to 3, "ruof" to 4, "evif" to 5, "xis" to 6, "neves" to 7, "thgie" to 8, "enin" to 9)
 
-    fun indexOfWordNumOneTwoSix(input: String): Any {
-        for (index in 0..input.length-3) {
-            if (numForward.contains(input.substring(index,index+3))) {
-                return listOf(index,input.substring(index,index+3));
-            }
-        }
-        return -1
-    }
-
-    fun indexOfWordNumFourFiveNine(input: String): Any {
-        for (i in 0..input.length-4) {
-            if (numForward.contains(input.substring(i,i+4))) {
-                return listOf(i, input.substring(i,i+4))
-            }
-        }
-        return -1
-    }
-
-    fun indexOfWordNumThreeSevenEight(input: String): Any {
-        for (i in 0..input.length-5) {
-            if (numForward.contains(input.substring(i+5))) {
-                return listOf(i, input.substring(i,i+5))
-            }
-        }
-        return -1
-    }
-
-    fun indexOfWordNumOneTwoSixReversed(input: String): Any {
-        for (index in 0..input.length-3) {
-            if (numBackward.contains(input.substring(index,index+3))) {
-                return listOf(index, input.substring(index,index+3));
-            }
-        }
-        return -1
-    }
-
-    fun indexOfWordNumFourFiveNineReversed(input: String): Any {
-        for (i in 0..input.length-4) {
-            if (numBackward.contains(input.substring(i,i+4))) {
-                return listOf(i, input.substring(i,i+4))
-            }
-        }
-        return -1
-    }
-
-    fun indexOfWordNumThreeSevenEightReversed(input: String): Any {
-        for (i in 0..input.length-5) {
-            if (numBackward.contains(input.substring(i+5))) {
-                return listOf(i, input.substring(i,i+5))
-            }
-        }
-        return -1
-    }
-
-
     fun part2(input: List<String>): Int {
-
         var result = 0
-
-        var num1: Int
-        var indexOfNum1 = -1
-        var indexOfNum1Word = -1
-        var num1Found = false
-        var num2: Int
-        var indexOfNum2 = -1
-        var indexOfNum2Word = -1
-        var num2Found = false
-
+        var num1 = 0
+        var num2 = 0
         for (element in input) {
-            for (index in 0..element.length-1) {
-                if (element[index].isDigit()) {
-                    num1 = element[index].toInt() * 10
-                    indexOfNum1 = index
-                    num1Found = true
+            num1 = 0
+            num2 = 0
+            for (i in 0 until element.length) {
+                if (element[i].isDigit()) {
+                    num1 = element[i].toString().toInt() * 10
+                    break
+                }
+                if (i+3 < element.length && element.substring(i, i+3) == "one") {
+                    num1 = 10
+                    break
+                }
+                if (i+3 < element.length && element.substring(i,i+3) == "two") {
+                    num1 = 20
+                    break
+                }
+                if (i+5 < element.length && element.substring(i,i+5) == "three") {
+                    num1 = 30
+                    break
+                }
+                if (i+4 < element.length && element.substring(i, i+4) == "four") {
+                    num1 = 40
+                    break
+                }
+                if (i+4 < element.length && element.substring(i, i+4) == "five") {
+                    num1 = 50
+                    break
+                }
+                if (i+3 < element.length && element.substring(i, i+3) == "six") {
+                    num1 = 60
+                    break
+                }
+                if (i+5 < element.length && element.substring(i, i+5) == "seven") {
+                    num1 = 70
+                    break
+                }
+                if (i+5 < element.length && element.substring(i, i+5) == "eight") {
+                    num1 = 80
+                    break
+                }
+                if (i+4 < element.length && element.substring(i, i+4) == "nine") {
+                    num1 = 90
                     break
                 }
             }
-            if (!num1Found) {
-                for (index in 0..element.length) {
-                    var numWord: String
-                    if (indexOfWordNumOneTwoSix(element) < indexOfNum1) {
-                        numWord =
-                        break
-                    }
-                    if (indexOfWordNumFourFiveNine(element) < indexOfNum1) {
-                        indexOfNum1 = indexOfWordNumFourFiveNine(element)
-                        break
-                    }
-                    if (indexOfWordNumThreeSevenEight(element) < indexOfNum1) {
-                        indexOfNum1 = indexOfWordNumThreeSevenEight(element)
-                        break
-                    }
+            //go backwards now
+            val elementRev = element.reversed()
+            for (i in element.length-1 downTo 0) {
+                if (element[i].isDigit()) {
+                    num2 = element[i].toString().toInt()
+                    break
                 }
-
-            }
-
-
-            val reversedString = element.reversed()
-
-            for (index in 0..reversedString.length-1) {
-                if (reversedString[index].isDigit()) {
-                    num2 = reversedString[index].toInt()
-                    indexOfNum2 = index
-                    num2Found = true
+                if (i-3 >= 0 && elementRev.substring(i-3,i) == "one".reversed()) {
+                    num2= 1
+                    break
+                }
+                if (i-3 >= 0 && elementRev.substring(i-3,i) == "two".reversed())  {
+                    num2 = 2
+                    break
+                }
+                if (i-5 >= 0 && elementRev.substring(i-5,i) == "three".reversed()) {
+                    num2 = 3
+                    break
+                }
+                if (i-4 >= 0 && elementRev.substring(i-4, i) == "four".reversed()) {
+                    num2 = 4
+                    break
+                }
+                if (i-4 >= 0 && elementRev.substring(i-4, i) == "five".reversed()) {
+                    num2 = 5
+                    break
+                }
+                if (i-3 >= 0 && elementRev.substring(i-3, i) == "six".reversed()) {
+                    num2 = 6
+                    break
+                }
+                if (i-5 >= 0 && elementRev.substring(i-5, i) == "seven".reversed()) {
+                    num2 = 7
+                    break
+                }
+                if (i-5 >= 0 && elementRev.substring(i-5, i) == "eight".reversed()) {
+                    num2 = 8
+                    break
+                }
+                if (i-4 >= 0 && elementRev.substring(i-4, i) == "nine".reversed()) {
+                    num2 = 9
                     break
                 }
             }
-            if (!num2Found) {
-                for (index in 0..reversedString.length) {
-                    if (indexOfWordNumOneTwoSixReversed(element) != -1) {
-                        indexOfNum2Word = indexOfWordNumOneTwoSixReversed(element)
-                        break
-                    }
-                    if (indexOfWordNumFourFiveNineReversed(element) != -1) {
-                        indexOfNum2Word = indexOfWordNumFourFiveNineReversed(element)
-                        break
-                    }
-                    if (indexOfWordNumThreeSevenEightReversed(element) != -1) {
-                        indexOfNum2Word = indexOfWordNumThreeSevenEightReversed(element)
-                        break
-                    }
-                }
-                num2 = reversedString[indexOfNum2]
-            }
-
-
-
-
+            result += num1 + num2
+            println(num1 + num2)
         }
-
-        return input.size
+        return result
     }
 
 
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
 
-    val input = readInput("Day01")
-    //part2(input).println()
+    val input = readInput("codeTest")
+    part2(input).println()
 }
